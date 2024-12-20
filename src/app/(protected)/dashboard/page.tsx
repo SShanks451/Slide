df51fe1594b2cea72ac7268030fe3f6759ea8048
Shashank Suman <shashanksuman@Shashanks-MacBook-Air-2.local>
2024-12-20 19:42:06 +0530

    set up auth and added sidebar

---
+++ src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {};
+
+const Page = (props: Props) => {
+  // WIP: Server Action Onboard the user
+  // WIP: 200 || 201
+
+  return <div>Page</div>;
+};
+
+export default Page;
